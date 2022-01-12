604b1430422e9b1af7ae294a11db2c888870c4a5
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-01-12 04:41:03 +0100

    Added null checks

--- addons/Discord RPC/ipc/pipe/WindowsPipe.gd
+++ addons/Discord RPC/ipc/pipe/WindowsPipe.gd
@@ -7,6 +7,9 @@ func open(path: String) -> int:
 	return self._file.open(path, File.READ_WRITE)
 
 func read() -> Array:
+	if not is_open():
+		return [-1, PoolByteArray()]
+	
 	var op_code: int = self._file.get_32()
 	var length: int = self._file.get_32()
 	var buffer: PoolByteArray = self._file.get_buffer(length)
@@ -14,17 +17,19 @@ func read() -> Array:
 	return [op_code, buffer]
 
 func write(bytes: PoolByteArray) -> void:
-	self._file.store_buffer(bytes)
+	if is_open():
+		_file.store_buffer(bytes)
 
 func is_open() -> bool:
 	return _file and _file.is_open()
 
 func has_reading() -> bool:
-	return self._file.get_len() > 0
+	return _file.get_len() > 0 if _file else false
 
 func close() -> void:
-	self._file.close()
-	self._file = null
+	if _file and _file.is_open():
+		_file.close()
+	_file = null
 
 func _to_string() -> String:
 	return "[WindowsPipe:%d]" % self.get_instance_id()
